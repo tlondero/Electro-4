d8b39909edded5f67350252653883165e34d2728
Guido <glambertucci@itba.edu.ar>
2021-03-31 17:52:05 -0300

    Grafiocs spice y teoricos

--- TP1/Ejercicio 1/Python/graphs.py
+++ TP1/Ejercicio 1/Python/graphs.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as signal
@@ -13,12 +14,21 @@ D=0.5
 Cgs_plus_cgd1=750E-12
 Cgs_plus_cgd2=1150E-12
 
+
+
 VTH=4
 VGSO=12
-VGSIO=6#mirar
-VD0=12
-VDSON=0
+VGSIO=5.5
+VD0=13
+
+IGSO=12/RG
+ION= VGSO/(RL+RDS)*D
+IOFF=ION
+IGSIO=65E-3
+
+VDSON=RDS*ION
 deltaQ=7.3E-9#fijate que en el spice dice otra cosa
+
 #Constantes de tiempo
 
 tau1=RG*Cgs_plus_cgd1
@@ -29,19 +39,18 @@ ton=-tau1*np.log(1-VTH/VGSO)#30.41E-9
 tri=-tau1*np.log((VGSO-VGSIO)/(VGSO)) - ton#5.198e-08-ton
 tvf=deltaQ*RG/(VGSO-VGSIO) #1.7358e-07-tri-ton
 
-tend=5.7358e-07
+tend=9.7358e-07
 
 
-IGSO=12/RG
-ION= VGSO/(RL+RDS)*D
-IOFF=ION
+
 
 t1= np.linspace(0,ton,100)
 t2= np.linspace(ton,tri+ton,100)
 t3= np.linspace(ton+tri,tvf+tri+ton,100)
 t4= np.linspace(tvf+tri+ton,tend,100)
-tvf1=np.linspace(tri+ton,tvf*0.2+tri+ton,int(100*0.2))
-tvf2=np.linspace(tvf*0.2+tri+ton,tvf+tri+ton,int(100*0.8))
+tvf1=np.linspace(tri+ton,tvf*1+tri+ton,int(100*1))
+#tvf2=np.linspace(tvf*0.5+tri+ton,tvf+tri+ton,int(100*0.5))
+
 
 VGS=[]
 
@@ -49,24 +58,11 @@ VGS1=(VGSO*(1-np.exp(-t1/tau1)))
 
 VGS2=(VGSO*(1-np.exp(-t2/tau1)))
 
-VGS3= 6+0*t3
+VGS3= VGSIO+0*t3
 
-delta=1.7358e-07
-VGS4=(6*(1-np.exp(-(t4-delta)/tau2)))+6
+delta=tvf+tri+ton
+VGS4=((VGSO-VGSIO)*(1-np.exp(-(t4-delta)/tau2)))+VGSIO
 
-
-plt.figure(num=1, figsize=(15, 5), dpi=80, facecolor='w', edgecolor='k')
-plt.plot(t1,VGS1,color='c')
-plt.plot(t2,VGS2,color='r')
-plt.plot(t3,VGS3,color='g')
-plt.plot(t4,VGS4,color='b')#, label='')
-
-plt.ylabel("VGS")
-plt.xlabel("t")
-plt.minorticks_on()
-plt.grid(which='major')
-plt.grid(which='minor')
-plt.show()
 
 IGS=[]
 
@@ -74,46 +70,27 @@ IGS1=(IGSO*(np.exp(-t1/tau1)))
 
 IGS2=(IGSO*(np.exp(-t2/tau1)))
 
-IGS3= 60e-3+0*t3
+IGS3= IGSIO+0*t3
 
-delta=1.7358e-07
-IGS4=(60e-3*(np.exp(-(t4-delta)/tau2)))
 
-plt.figure(num=2, figsize=(15, 5), dpi=80, facecolor='w', edgecolor='k')
-plt.plot(t1,IGS1,color='c')
-plt.plot(t2,IGS2,color='r')
-plt.plot(t3,IGS3,color='g')
-plt.plot(t4,IGS4,color='b')#, label='')
+IGS4=(IGSIO*(np.exp(-(t4-delta)/tau2)))
 
-plt.ylabel("IGS ")
-plt.xlabel("t")
-plt.minorticks_on()
-plt.grid(which='major')
-plt.grid(which='minor')
-plt.show()
+
+
+
 
 
 IDS=[]
 
 IDS1=(0*(np.exp(-t1/tau1)))
 
-IDS2=(ION*(t2-ton)/(5.198e-08-30.41E-9))
+IDS2=(ION*(t2-ton)/(tri))
 
 IDS3=ION+t3*0
 
 IDS4=ION+t4*0
 
-plt.figure(num=3, figsize=(15, 5), dpi=80, facecolor='w', edgecolor='k')
-plt.plot(t1,IDS1,color='c')
-plt.plot(t2,IDS2,color='r')
-plt.plot(t3,IDS3,color='g')
 
-plt.ylabel("IDS ")
-plt.xlabel("t")
-plt.minorticks_on()
-plt.grid(which='major')
-plt.grid(which='minor')
-plt.show()
 
 VDS=[]
 
@@ -122,25 +99,67 @@ VDS1=(VD0+0*(np.exp(-t1/tau1)))
 
 VDS2=VDS1
 
-VDS3=(VD0-(VD0*(tvf1-(ton+tri))/(tvf*0.2)))
+VDS3=((VD0)-((VD0-VDSON)*(tvf1-(ton+tri))/(tvf)))
 
-VDS4=tvf2*VDSON
-VDS5=t4*VDSON
+#VDS4=tvf2*VDSON
+VDS5=t4*0+VDSON
 
 
-plt.figure(num=4, figsize=(15, 5), dpi=80, facecolor='w', edgecolor='k')
-plt.plot(t1,VDS1,color='c')
-plt.plot(t2,VDS2,color='r')
+fig, ax1 = plt.subplots()
+ax1.set_xlabel('time (s)')
+ax1.set_ylabel('Vds')
+ax1.plot(t1, VDS1, color='c')
+ax1.plot(t2, VDS2, color='r')
+ax1.plot(tvf1, VDS3, color='g')
+ax1.plot(t4, VDS5, color='g',label='VDS')
+plt.legend()
 
-plt.plot(tvf1,VDS3,color='g')
-plt.plot(tvf2,VDS4,color='b')
-plt.plot(t4,VDS5,color='g')
-plt.ylabel("VDS")
+
+
+
+ax2 = ax1.twinx()
+ax2.set_ylabel('Ids')
+ax2.plot(t1,IDS1,'--',color='c')
+ax2.plot(t2,IDS2,'--',color='r')
+ax2.plot(t3,IDS3,'--',color='g',label='IDS')
+ax2.plot(t4,IDS3,'--',color='g')
+
 plt.xlabel("t")
+plt.legend()
 plt.minorticks_on()
 plt.grid(which='major')
 plt.grid(which='minor')
-#plt.legend()
+plt.show()
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.show()
 
+
+
+fig2, ax3 = plt.subplots()
+ax3.set_xlabel('time (s)')
+ax3.set_ylabel('Vgs')
+
+ax3.plot(t1,VGS1,color='c')
+ax3.plot(t2,VGS2,color='r')
+ax3.plot(t3,VGS3,color='g')
+ax3.plot(t4,VGS4,color='b',label='VGS')#, label='')
+
+plt.legend()
+plt.minorticks_on()
+plt.grid(which='major')
+plt.grid(which='minor')
+
+
+
+
+ax4 = ax3.twinx()
+ax4.set_ylabel('Igs')
+
+ax4.plot(t1,IGS1,'--',color='c')
+ax4.plot(t2,IGS2,'--',color='r')
+ax4.plot(t3,IGS3,'--',color='g')
+ax4.plot(t4,IGS4,'--',color='b',label='IGS')#, label='')
+plt.legend()
+plt.xlabel("t")
+plt.show()
 
