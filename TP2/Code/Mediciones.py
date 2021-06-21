8367ebab091d09d749c45f3106f2f689115c3f73
glambertucci <glambertucci@itba.edu.ar>
2021-06-21 11:32:15 -0300

    Fotos

--- TP2/Code/Mediciones.py
+++ TP2/Code/Mediciones.py
@@ -4,10 +4,10 @@ import pandas as pnd
 
 #CSV PARSER
 
-df = pnd.read_csv('../Mediciones/Pin9-Vref2V.csv', sep=',')
-t = np.asarray(df["x-axis"])
+df = pnd.read_csv('../Mediciones/Vsec-Vref2V.csv', sep=',')
+t = np.asarray(df["Time (s)"])
 t = (t - t[0])*(1E6)
-pin9 = np.asarray(pnd.DataFrame(df["1"]).ewm(com=100).mean())
+pin9 = np.asarray(pnd.DataFrame(df["Channel 1 (V)"]).ewm(com=100).mean())
 
 #TENSIÓN DE COM
 plt.figure(num=4, figsize=(15, 5), dpi=80, facecolor='w', edgecolor='k')
@@ -15,9 +15,9 @@ plt.ylabel("Tensión [V]")
 plt.xlabel("Tiempo $[\mu s]$")
 plt.minorticks_on()
 plt.grid(which='major')
-#plt.grid(which='minor')
-plt.plot(t,pin9, label="Tensión de Pin 9")
+
+plt.plot(t,pin9, label="Tensión de secundario")
 plt.legend()
 plt.xlim(0, 50)
-#plt.savefig('..\Tex\ParteIV\ImagenesParteIV\Vcom.png')
+plt.savefig('..\Tex\ParteIV\ImagenesParteIV\Vsec.png')
 plt.show()
