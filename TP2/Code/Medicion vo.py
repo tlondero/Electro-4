490c1205634c3b5336b2d714bbd2d0fa651f3942
glambertucci <glambertucci@itba.edu.ar>
2021-06-22 09:27:46 -0300

    saque el pipipip

---
+++ TP2/Code/Medicion vo.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pnd
+
+#CSV PARSER
+
+
+
+
+#TENSIÓN DE SALIDA
+df = pnd.read_csv('../Mediciones/1127/Vo-Vref1127V.csv', sep=',')
+t = np.asarray(df["Time (s)"])
+t = (t - t[0])*(1E6)
+#pin9 = np.asarray(df["Channel 1 (V)"])*0.7+0.915
+pin9 = np.asarray(df["Channel 1 (V)"])#+0.014
+plt.figure(num=4, figsize=(15, 5), dpi=80, facecolor='w', edgecolor='k')
+plt.ylabel("Tensión [V]")
+plt.xlabel("Tiempo $[\mu s]$")
+plt.minorticks_on()
+plt.grid(which='major')
+#vline1=t*0+3.075
+#vline2=t*0+2.925
+
+vline1 = t * 0 + 3.15
+vline2 = t * 0 + 2.85
+
+plt.plot(t,pin9, label="Tensión de salida   ")
+plt.plot(t,vline2,'--' )
+plt.plot(t,vline1,'--')
+
+plt.legend()
+plt.xlim(0, 50)
+plt.show()
--- TP2/Code/Mediciones.py
+++ TP2/Code/Mediciones.py
@@ -76,18 +76,18 @@ plt.show()
 df = pnd.read_csv('../Mediciones/1127/Vo-Vref1127V.csv', sep=',')
 t = np.asarray(df["Time (s)"])
 t = (t - t[0])*(1E6)
-pin9 = np.asarray(df["Channel 1 (V)"])*0.7+0.915
-#pin9 = np.asarray(df["Channel 1 (V)"])#+0.014
+#pin9 = np.asarray(df["Channel 1 (V)"])*0.7+0.915
+pin9 = np.asarray(df["Channel 1 (V)"])#+0.014
 plt.figure(num=4, figsize=(15, 5), dpi=80, facecolor='w', edgecolor='k')
 plt.ylabel("Tensión [V]")
 plt.xlabel("Tiempo $[\mu s]$")
 plt.minorticks_on()
 plt.grid(which='major')
-vline1=t*0+3.075
-vline2=t*0+2.925
+#vline1=t*0+3.075
+#vline2=t*0+2.925
 plt.plot(t,pin9, label="Tensión de salida   ")
-plt.plot(t,vline2,'--' )
-plt.plot(t,vline1,'--')
+#plt.plot(t,vline2,'--' )
+#plt.plot(t,vline1,'--')
 
 plt.legend()
 plt.xlim(0, 50)
